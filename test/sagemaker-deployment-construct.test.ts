c512841d3bc8d2a50a2f56c44a7b5663d962e38a
abidkhan03 <abidkhan42935@gmail.com>
2025-03-05 17:33:42 +0500

    Initialize cdk construct

---
+++ test/sagemaker-deployment-construct.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SagemakerDeploymentConstruct from '../lib/sagemaker-deployment-construct-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sagemaker-deployment-construct-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SagemakerDeploymentConstruct.SagemakerDeploymentConstructStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
